8eb64daeba0a37cef8ec84a222ff688dd42f014a
Comamoca <comamoca.dev@gmail.com>
2023-01-23 20:53:13 +0900

    :beetle: (/src/deps.ts) Change the loading destination of ky to esm.sh

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
 export { join } from "https://deno.land/std@0.130.0/path/mod.ts";
-export { default as ky } from "https://cdn.skypack.dev/ky?dts";
+export { default as ky } from "https://esm.sh/ky";
 export { addParams } from "https://deno.land/x/add_params@v1.2.0/mod.ts";
 export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 export { encodeUrl } from "https://deno.land/x/encodeurl/mod.ts";
